9d18759d0fbf4eefcea68e11949508f8c9067fc4
jasonsum <jasummer92@gmail.com>
2022-05-28 16:13:45 -0500

    Add initial unit tests for cleaning module

---
+++ test/test_pivot_measures.py
@@ -0,0 +1,68 @@
+"""
+Tests the pivot_measures function in clean module.
+"""
+
+import pytest
+
+import numpy as np
+import pandas as pd
+
+from src.clean import pivot_measures
+
+# Define input dataframe
+df_in_values = [['Ohio', 'Summit', 39153, 39153503300, 5606,
+        "{'type': 'Point', 'coordinates': [-81.49716013, 41.04049422]}",
+        'MHLTH', 21.7, 'Health Status', 'Mental Health',
+        'Mental health not good for >=14 days among adults aged >=18 years'],
+       ['South Carolina', 'Orangeburg', 45075, 45075010200, 5097,
+        "{'type': 'Point', 'coordinates': [-80.38981016, 33.3179319]}",
+        'GHLTH', 30.5, 'Health Status', 'General Health',
+        'Fair or poor self-rated health status among adults aged >=18 years'],
+       ['Michigan', 'Wayne', 26163, 26163543900, 901,
+        "{'type': 'Point', 'coordinates': [-83.2493016, 42.38181837]}",
+        'COPD', 14.2, 'Health Outcomes', 'COPD',
+        'Chronic obstructive pulmonary disease among adults aged >=18 years']]
+df_in_index = [1114538, 1255710, 766068]
+df_in_columns = ['StateDesc', 'CountyName', 'CountyFIPS', 'LocationID',
+       'TotalPopulation', 'Geolocation', 'MeasureId', 'Data_Value', 'Category',
+       'Short_Question_Text', 'Measure']
+df_in = pd.DataFrame(df_in_values, index=df_in_index, columns=df_in_columns)
+
+def test_pivot_measures():
+    """
+    Conducts happy path unit test for pivot_measures function.
+    """
+
+    # Define expected output
+    df_true = pd.DataFrame(
+            [['Michigan', 'Wayne', 26163, 26163543900, 901,
+        "{'type': 'Point', 'coordinates': [-83.2493016, 42.38181837]}",
+        14.2, None, None],
+       ['Ohio', 'Summit', 39153, 39153503300, 5606,
+        "{'type': 'Point', 'coordinates': [-81.49716013, 41.04049422]}",
+        None, None, 21.7],
+       ['South Carolina', 'Orangeburg', 45075, 45075010200, 5097,
+        "{'type': 'Point', 'coordinates': [-80.38981016, 33.3179319]}",
+        None, 30.5, None]],
+            index = [0,1,2],
+            columns = ['StateDesc', 'CountyName', 'CountyFIPS', 'LocationID',
+                        'TotalPopulation', 'Geolocation', 'COPD', 'GHLTH', 'MHLTH'])
+
+    # Create test output
+    df_test = pivot_measures(df_in)
+
+    # Test equality
+    pd.testing.assert_frame_equal(df_true, df_test)
+
+def test_pivot_measures_key_err():
+    """
+    Conducts unhappy path unit test for pivot_measures function.
+
+    Checks if KeyError raised for missing column.
+    """
+
+    # Create test output
+    with pytest.raises(KeyError):
+        pivot_measures(df_in.drop(['MeasureId']))
+
+
---
+++ test/test_validate_df.py
@@ -0,0 +1,79 @@
+"""
+Tests the validate_df function in clean module.
+"""
+
+import pytest
+
+import numpy as np
+import pandas as pd
+
+from src.clean import validate_df
+
+# Define input dataframe
+df_in_values = [['Massachusetts', 'Hampden', 25013, 25013812500, 7665,
+        "{'type': 'Point', 'coordinates': [-72.70927126, 42.14739821]}",
+        'CSMOKING', 18.8, 'Health Risk Behaviors', 'Current Smoking',
+        'Current smoking among adults aged >=18 years'],
+       ['Illinois', 'Cook', 17031, 17031400300, 1567,
+        "{'type': 'Point', 'coordinates': [-87.61316825, 41.79314226]}",
+        'DEPRESSION', 18.3, 'Health Outcomes', 'Depression',
+        'Depression among adults aged >=18 years'],
+       ['Virginia', 'Arlington', 51013, 51013101404, 4255,
+        "{'type': 'Point', 'coordinates': [-77.10506054, 38.88292427]}",
+        'OBESITY', 19.1, 'Health Outcomes', 'Obesity',
+        'Obesity among adults aged >=18 years']]
+df_in_index = [687146, 471561, 1432558]
+df_in_columns = ['StateDesc', 
+                             'CountyName', 
+                             'CountyFIPS', 
+                             'LocationID', 
+                             'TotalPopulation', 
+                             'Geolocation', 
+                             'MeasureId', 
+                             'Data_Value', 
+                             'Category', 
+                             'Short_Question_Text', 
+                             'Measure']
+df_in = pd.DataFrame(df_in_values, index=df_in_index, columns=df_in_columns)
+
+def test_validate_df():
+    """
+    Conducts happy path unit test for validate_df function.
+    """
+
+    # Create test output
+    assert validate_df(df_in,
+                          cols = {"StateDesc":"object",
+                                  "CountyName":"object",
+                                  "CountyFIPS":"int",
+                                  "LocationID":"int",
+                                  "TotalPopulation":"int",
+                                  "Geolocation":"object",
+                                  "MeasureId":"object",
+                                  "Data_Value":"float",
+                                  "Category":"object",
+                                  "Short_Question_Text":"object",
+                                  "Measure":"object"}
+                                  ) is None
+
+def test_validate_df_type_err():
+    """
+    Conducts unhappy path unit test for validate_df function.
+
+    Checks for ValueError if data type mismatch occurs.
+    """
+
+    # Create test output
+    with pytest.raises(TypeError):
+        validate_df(df_in,
+                    cols = {"StateDesc":"int", # Should be object
+                                    "CountyName":"object",
+                                    "CountyFIPS":"int",
+                                    "LocationID":"int",
+                                    "TotalPopulation":"int",
+                                    "Geolocation":"object",
+                                    "MeasureId":"object",
+                                    "Data_Value":"object",
+                                    "Category":"object",
+                                    "Short_Question_Text":"object",
+                                    "Measure":"object"})
